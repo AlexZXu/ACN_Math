655c0854826ea073d4fd2fbd1d5d6e0fffe3c5a7
AlexZXu <102630459+AlexZXu@users.noreply.github.com>
2022-06-03 22:17:15 -0400

    Create prototypeACN.java
    Created first prototype to create an ACN object. Takes in string as input.

---
+++ prototypeACN.java
@@ -0,0 +1,54 @@
+public class ACN_1 {
+	static class ACN {
+		static int ALLOCATEDWHOLE, ALLOCATEDDECIMAL;
+		static int [] WHOLE, DECIMAL;
+		
+		ACN (String num) {
+			ALLOCATEDWHOLE = 1000;
+			ALLOCATEDDECIMAL = 1000;
+			
+			WHOLE = new int [ALLOCATEDWHOLE];
+			DECIMAL = new int [ALLOCATEDDECIMAL];
+			
+			try {
+				setArrays(num);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		private void setArrays (String num) throws Exception {
+			boolean seenDot = false;
+			int pointerA = 0;
+			int pointerB = 0;
+			for (int i = 0; i < num.length(); i++) {
+				if (Character.isDigit(num.charAt(i))) {
+					if (seenDot == false) {
+						WHOLE [pointerA] = Integer.parseInt(num.charAt(i) + "");
+						pointerA++;
+					}
+					else if (seenDot == true) {
+						DECIMAL [pointerB] = Integer.parseInt(num.charAt(i) + "");
+					}
+					
+				}
+				else if (seenDot == false && num.charAt(i) == '.') {
+					seenDot = true;
+				}
+				else if (seenDot == true && num.charAt(i) == '.') {
+					throw new Exception ("Error in input: Input contains more than one decimal point");
+				}
+				
+				else {
+					throw new Exception ("Error in input: Input contains characters that are not digits");
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		ACN test = new ACN ("5.45.f");
+	}
+
+}
